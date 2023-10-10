97968cef447447a9e102f6a3989312a63ecbf1d8
Ari625 <arir06101@gmail.com>
2023-10-10 07:43:24 +0700

    10-10-2023

--- sekolah/index.php
+++ sekolah/index.php
@@ -6,7 +6,7 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Sistem Informasi Akademik SMK Damay</title>
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-   <link href="sign-in.css" rel="stylesheet">
+   <link href="css/sign-in.css" rel="stylesheet">
 </head>
 <body>
    <h2 class="text-center text-primary mt-2">Sistem Informasi</h2>
