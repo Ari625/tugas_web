fa3f6134779271a77b935aa4b69d2db89386726e
Ari625 <arir06101@gmail.com>
2023-10-30 21:42:20 +0700

    30-10-2023

--- penjualan/index.php
+++ penjualan/index.php
@@ -1,8 +1,9 @@
 <?php
+session_start();
 require "functions_penjualan.php";
 $barang = query("SELECT * FROM barang");
 
-if (isset($_POST["cari"])){
+if (isset($_POST["cari"])) {
    $barang = cari($_POST["keyword"]);
 }
 ?>
@@ -13,7 +14,8 @@ if (isset($_POST["cari"])){
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
    <title>Penjualan</title>
 </head>
 
@@ -21,6 +23,26 @@ if (isset($_POST["cari"])){
    <h2 class="text-center">
       Data Barang
    </h2>
+
+   <div class="d-flex flex-row-reverse">
+      <?php if (isset($_SESSION["login"])): ?>
+         <div class="position-relative top-0 end-0">
+            <a href="logout.php">
+               <button class="btn btn-primary">
+                  Logout
+               </button>
+            </a>
+         </div>
+      <?php else: ?>
+         <div class="position-relative top-0 end-0">
+            <a href="login.php">
+               <button class="btn btn-primary">
+                  Login
+               </button>
+            </a>
+         </div>
+      <?php endif ?>
+   </div>
    <hr>
    <form action="" method="POST" class="mb-3">
       <input type="text" name="keyword" size="40" autofocus placeholder="Masukan Keyword Pencarian.."
@@ -28,57 +50,104 @@ if (isset($_POST["cari"])){
       <button type="submit" name="cari" class="btn btn-secondary">Cari!</button>
    </form>
 
-   <table class="table-bordered" border="1" cellspacing="1" cellpading="5">
-      <tr>
-         <th class="text-center">No.</th>
-         <th class="text-center">Kode Barang</th>
-         <th class="text-center">Nama Barang</th>
-         <th class="text-center">Satuan</th>
-         <th class="text-center">Merk</th>
-         <th class="text-center">Harga</th>
-         <th class="text-center">Aksi</th>
-      </tr>
-      <?php $i = 1 ?>
-      <?php foreach ($barang as $row): ?>
+   <?php if (isset($_SESSION["login"])): ?>
+      <table class="table-bordered" border="1" cellspacing="1" cellpading="5">
          <tr>
-            <td class="">
-               <?= $i; ?>
-            </td>
-            <td>
-               <?= $row["kode_barang"]; ?>
-            </td>
-            <td>
-               <?= $row["nama_barang"]; ?>
-            </td>
-            <td>
-               <?= $row["satuan"]; ?>
-            </td>
-            <td>
-               <?= $row["merk"]; ?>
-            </td>
-            <td>
-               <?= $row["harga"]; ?>
-            </td>
-            <td>
-               <a href="update.php?kode_barang=<?= $row["kode_barang"]; ?>" class="text-primary btn">
-                  Ubah
-               </a>
-               |
-               <a href="delete.php?kode_barang=<?= $row["kode_barang"]; ?>" onclick="return confirm('yakin?');"
-                  class="text-danger btn">
-                  Hapus
-               </a>
-            </td>
+            <th class="text-center">No.</th>
+            <th class="text-center">Kode Barang</th>
+            <th class="text-center">Nama Barang</th>
+            <th class="text-center">Satuan</th>
+            <th class="text-center">Merk</th>
+            <th class="text-center">Harga</th>
+            <?php if ($_SESSION["role"] === "admin"): ?>
+               <th class="text-center">Aksi</th>
+
+            <?php else: ?>
+
+            <? endif ?>
          </tr>
-         <?php $i++; ?>
-      <?php endforeach ?>
-   </table>
-   
-   <a href="create.php" class="btn">
-      <button class="btn btn-primary">
-         Tambah
-      </button>
-   </a>
+         <?php $i = 1 ?>
+         <?php foreach ($barang as $row): ?>
+            <tr>
+               <td class="">
+                  <?= $i; ?>
+               </td>
+               <td>
+                  <?= $row["kode_barang"]; ?>
+               </td>
+               <td>
+                  <?= $row["nama_barang"]; ?>
+               </td>
+               <td>
+                  <?= $row["satuan"]; ?>
+               </td>
+               <td>
+                  <?= $row["merk"]; ?>
+               </td>
+               <td>
+                  <?= $row["harga"]; ?>
+               </td>
+               <?php if ($_SESSION["role"] === "admin"): ?>
+                  <td>
+                     <a href="update.php?kode_barang=<?= $row["kode_barang"]; ?>" class="text-primary btn">
+                        Ubah
+                     </a>
+                     |
+                     <a href="delete.php?kode_barang=<?= $row["kode_barang"]; ?>" onclick="return confirm('yakin?');"
+                        class="text-danger btn">
+                        Hapus
+                     </a>
+                  </td>
+               <?php else: ?>
+
+               <? endif ?>
+            </tr>
+            <?php $i++; ?>
+         <?php endforeach ?>
+      </table>
+
+      <?php if ($_SESSION["role"] === "admin" || $_SESSION["role"] === "default"): ?>
+         <a href="create.php" class="btn">
+            <button class="btn btn-primary">
+               Tambah
+            </button>
+         </a>
+      <?php else: ?>
+
+      <? endif ?>
+   <?php else: ?>
+
+      <table class="table-bordered" border="1" cellspacing="1" cellpading="5">
+         <tr>
+            <th class="text-center">No.</th>
+            <th class="text-center">Nama Barang</th>
+            <th class="text-center">Satuan</th>
+            <th class="text-center">Merk</th>
+            <th class="text-center">Harga</th>
+         </tr>
+         <?php $i = 1 ?>
+         <?php foreach ($barang as $row): ?>
+            <tr>
+               <td class="">
+                  <?= $i; ?>
+               </td>
+               <td>
+                  <?= $row["nama_barang"]; ?>
+               </td>
+               <td>
+                  <?= $row["satuan"]; ?>
+               </td>
+               <td>
+                  <?= $row["merk"]; ?>
+               </td>
+               <td>
+                  <?= $row["harga"]; ?>
+               </td>
+            </tr>
+            <?php $i++; ?>
+         <?php endforeach ?>
+      </table>
+   <?php endif ?>
 </body>
 
 </html>
---
+++ penjualan/login.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+require "functions_penjualan.php";
+
+if (isset($_POST["login"])) {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $result = mysqli_query($conn, "SELECT * FROM users WHERE email = '$email'");
+    if (mysqli_num_rows($result) === 1) {
+        $row = mysqli_fetch_array($result);
+        if (mysqli_query($conn,"SELECT * FROM users WHERE password = '$password'")) {
+            $_SESSION["login"] = true;
+            $_SESSION["role"] = $row["role"];
+            header("Location: index.php");
+            exit;
+        }
+    }
+    $error = true;
+    // var_dump($result);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Login</title>
+</head>
+
+<body>
+
+    <main class="form-signin w-50 m-auto">
+        <form method="post" action="">
+            <h1 class="h3 mb-3 fw-normal text-center">LOGIN</h1>
+
+            <?php if (isset($error)) : ?>
+                <p class="text-danger">
+                    email atau password salah
+                </p>
+            <?php endif ?>
+            <div class="form-floating">
+                <input type="email" class="form-control mb-1" id="email" placeholder="Masukan email" name="email">
+                <label for="email">Email</label>
+            </div>
+            <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password"
+                    name="password">
+                <label for="floatingPassword">Password</label>
+            </div>
+            <button class="btn btn-primary w-100 py-2" type="submit" name="login">LOGIN</button>
+            <p class="mt-5 mb-3 text-body-secondary">&copy;2023</p>
+        </form>
+    </main>
+</body>
+
+</html>
